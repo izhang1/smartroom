694be33616bc84a1cb27361c8f892db88d722b63
izhang1 <ivanzhangemail@gmail.com>
2016-03-18 12:32:57 -0400

    Initialized UI in the onCreate. Example of Volley given in same method

--- app/src/main/java/com/example/izhang/smartroom/MusicPlayer.java
+++ app/src/main/java/com/example/izhang/smartroom/MusicPlayer.java
@@ -2,6 +2,14 @@ package com.example.izhang.smartroom;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Spinner;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 public class MusicPlayer extends AppCompatActivity {
 
@@ -9,5 +17,37 @@ public class MusicPlayer extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_music_player);
+
+        Spinner musicList = (Spinner) this.findViewById(R.id.musicList);
+        Button playButton = (Button) this.findViewById(R.id.playSongButton);
+        Button pauseButton = (Button) this.findViewById(R.id.pauseSongButton);
+        Button stopButton = (Button) this.findViewById(R.id.stopSongButton);
+
+        String url = "http://httpbin.org/html";
+
+        // Request a string response
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                        // Result handling
+                        System.out.println(response.substring(0,100));
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+                // Error handling
+                System.out.println("Something went wrong!");
+                error.printStackTrace();
+
+            }
+        });
+
+        // Add the request to the queue
+        Volley.newRequestQueue(this).add(stringRequest);
+
     }
 }
