5d07372092e18ff2af9d51a4569bee4de00e05d6
izhang1 <ivanzhangemail@gmail.com>
2016-03-17 23:55:39 -0400

    Added new UI and created layout for MusicPlayer activity

---
+++ app/src/main/java/com/example/izhang/smartroom/MusicPlayer.java
@@ -0,0 +1,13 @@
+package com.example.izhang.smartroom;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MusicPlayer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_music_player);
+    }
+}
